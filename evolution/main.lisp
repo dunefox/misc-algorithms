31d410516774bc7fd446f689f33667adaf0e3ff4
dunefox <ph.fuchs@campus.lmu.de>
2019-08-31 19:35:07 +0200

    make-tree

--- evolution/main.lisp
+++ evolution/main.lisp
@@ -1,4 +1,12 @@
-(defparameter ops '(+ - *))
+(defparameter *ops* '(+ - * /))
+(defparameter *max-depth* 5)
+(defparameter *max-nodes* 5)
 
-(random 10)
+(defun make-tree (symbol depth)
+  (cond ((= depth 0) 1)
+        ((not (member symbol *ops*)) symbol)
+        (t (let* ((alpha (+ 1 (random *max-nodes*)))
+                  (elts (concatenate 'list *ops* (loop :for n :below 10 collect (+ 1 n))))
+                  (tree-level (loop for i from 1 to alpha collect (nth (+ 1 (random (length elts))) elts))))
+             (cons symbol (mapcar (lambda (x) (make-tree x (- 1 depth))) tree-level))))))
 
