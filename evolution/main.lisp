97ef1f713fb69914bc02224f3eb10f6a15975eee
dunefox <ph.fuchs@campus.lmu.de>
2019-09-04 00:15:17 +0200

    :del :tree :num work in mutate-tree

--- evolution/main.lisp
+++ evolution/main.lisp
@@ -4,6 +4,7 @@
 (defparameter *max-range* 5)
 (defparameter *numbers* (loop :for n :below *max-range* :collect (+ 1 n)))
 (defparameter *elts* (append *ops* *numbers*))
+(defparameter *mutations* '(:num :num :tree :tree :tree :del :ins :ins))
 
 (defun make-tree (symbol depth)
   (cond ((<= depth 0) (elt *numbers* (random *max-range*)))
@@ -18,8 +19,46 @@
       (let ((cleaned (remove-if-not #'listp form)))
         (cond ((eq cleaned '()) depth)
               (t (mapcar (lambda (x) (go_ x (1+ depth))) cleaned)))))
-                                        ;(setf depths '())
     (reduce #'max (flatten (go_ tree)))))
+
+(defun mutate-tree (tree)
+  (progn
+    (defparameter *d* (random (calc-depth tree)))
+    (defun go_ (form depth)
+      (let* ((action (elt *mutations* (random (length *mutations*))))
+             (rnd-ind (1+ (random (1- (length form)))))
+             (rnd-form (elt form rnd-ind)))
+        (progn
+          (print (format nil "form: ~A" form))
+          (print (format nil "depth: ~A" depth))
+          (print (format nil "ind: ~A" rnd-ind))
+          (print (format nil "rnd-form: ~A" rnd-form))
+          (print (format nil "action: ~A" action))
+          (cond
+            ((and (= 1 (length form))
+                  (member (car form) *ops*))
+             (return-from go_ :dead))
+            ((and (> depth 0)
+                  (>= (random 1.0) 0.3)
+                  (listp rnd-form))
+             (go_ rnd-form (- depth 1)))
+            ((eq action :tree) (setf (elt form rnd-ind) (make-tree (elt *ops* (random (length *ops*))) 1)))
+            ((eq action :num)  (setf (elt form rnd-ind) (random *max-range*)))
+            ((eq action :del)  (progn
+                                 (setf (elt form rnd-ind) nil)
+                                 (delete nil form)
+                                 form))
+            ((eq action :ins)  (nconc (make-tree (elt *ops* (random (length *ops*))) (1- (random *max-depth*))) (list form)))
+            (t (print "else"))))))
+    (print tree)
+    (go_ tree *d*)))
+
+;; TODO: mutate, select, fitness, crossover
+;; mutate: select random node, swap children for (make-tree rand-symb (- *max-depth* calc-depth))
+;;      || delete subtrees, replace with number
+;;      || select random number, replace with new make-tree
+;; TODO: Random symbol for root of tree
+;; TODO: refactor go_ into generic walk-tree?
 
 (defun flatten (l)
   (cond ((null l) nil)
